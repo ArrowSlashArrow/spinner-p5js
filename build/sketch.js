4aad6e29cd882f8948eaab314800b4a172cb35f0
ArrowSlashArrow <153234615+ArrowSlashArrow@users.noreply.github.com>
2024-08-25 20:55:37 -0600

    fixed various bugs
    fixed the following bugs:
     - angle not properly corresponding to total_time 
     - custom font not loading
    
    [note]: this will only work on a http server

--- build/sketch.js
+++ build/sketch.js
@@ -2,8 +2,9 @@ let WIDTH = window.innerWidth;
 let HEIGHT = window.innerHeight;
 
 const TO_RADIANS = Math.PI / 180;
+const SCALE = 1;
 
-let total_time = 60.0;
+let total_time = 600;
 
 let start_time = new Date();
 
@@ -14,7 +15,7 @@ let settings = {
 
 let font;
 function preload() {
-    font = loadFont("C:/Users/anton/Desktop/webdev/timer_bg/assets/JetBrainsMono-Italic.ttf")
+    font = loadFont('assets/JetBrainsMono-Italic.ttf') 
 }
 
 function format_time(seconds) {
@@ -31,6 +32,14 @@ function setup() {
     background(0);
     stroke(255, 85);
 
+    let url_params = new URLSearchParams(window.location.search);
+    
+    if ("time" in url_params) {
+        let time_param = url_params.get('time');
+        total_time = Number(time_param);
+        settings["line_count"] = Math.max(50.0, (total_time / 12.0));
+    }
+    
     textSize(40);
     textFont(font);
 
@@ -38,36 +47,34 @@ function setup() {
 
 function draw() {
     background(0);
-    var elapsed_time = (new Date - start_time + 100000) / 1000.0;
+    var elapsed_time = 360.0 * (new Date - start_time) / 1000.0 / total_time;
     var angle = elapsed_time * TO_RADIANS;
 
+    stroke(255, 85);
+    strokeWeight(1);
     
-    for (let i = 0; i < settings["line_count"]; i++) {
-        for (let j = 0; j < 3; j++) {
-            strokeWeight(j + 1);
-            let scale = (j * 2 / 3 + 1);
-            let x_start = scale * settings["size"] * Math.sin(angle * i) + WIDTH / 2.0;
-            let y_start = scale * settings["size"] * Math.cos(angle * i) + HEIGHT / 2.0;
-            let x_end = scale * settings["size"] * Math.sin(angle * (i + 1)) + WIDTH / 2.0;
-            let y_end = scale * settings["size"] * Math.cos(angle * (i + 1)) + HEIGHT / 2.0;
-            
-            line(x_start, y_start, x_end, y_end);
-        }
+    for (let i = 0; i < settings["line_count"]; i++) { 
+        let x_start = SCALE * settings["size"] * Math.sin(angle * i) + WIDTH / 2.0;
+        let y_start = SCALE * settings["size"] * Math.cos(angle * i) + HEIGHT / 2.0;
+        let x_end = SCALE * settings["size"] * Math.sin(angle * (i + 1)) + WIDTH / 2.0;
+        let y_end = SCALE * settings["size"] * Math.cos(angle * (i + 1)) + HEIGHT / 2.0;
         
+        line(x_start, y_start, x_end, y_end);
     }
 
-
+    textAlign(CENTER, CENTER);
     strokeWeight(4);
     fill(255);
     stroke(0);
     let current_time = new Date();
-    let time_str = current_time.toLocaleDateString() + "\n" + current_time.toLocaleTimeString();
+    let time_str = current_time.toLocaleDateString() + "\n" + current_time.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false});
     text(time_str, WIDTH / 2, HEIGHT / 2)
-    textAlign(CENTER, CENTER);
+    
     stroke(255, 85);
 }
 
-function windowResize() {
+function windowResized() {
+    resizeCanvas(window.innerWidth, window.innerHeight);
     WIDTH = window.innerWidth;
     HEIGHT = window.innerHeight;
 }
